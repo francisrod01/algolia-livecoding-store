69a6ffec21c0cd549f0442f98b6d085095e0e644
Francis Rodrigues <francis.rod01@outlook.com>
2021-08-16 20:29:25 -0300

    feat(Map): Implemented App using mapbox & algolia

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,50 @@
-import React from 'react';
+import { useState } from 'react';
+import { Configure, Hits, InstantSearch, RefinementList } from 'react-instantsearch-dom';
 
-import logo from './logo.svg';
+import { indexName, searchClient } from './lib/algoliaClient';
+import { GeoHit } from './types/StoreHit';
+
+import Header from './components/Header/Header';
+import StoreComponent from './components/StoreComponent/StoreComponent';
+import Map from './components/Map/Map';
+
 import './App.css';
 
 function App() {
+  const [currentStore, setCurrentStore] = useState<GeoHit | null>(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="font-medium text-2xl text-black">
-          Hello Algolia!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex w-full flex-col">
+
+      <Header />
+
+      <InstantSearch searchClient={searchClient} indexName={indexName}>
+        <Configure aroundLatLngViaIP={true} />
+
+        <div className="flex h-full w-full">
+          <div className="flex flex-col w-1/4">
+            <div className="m-2">
+              <RefinementList attribute={'services'} />
+            </div>
+
+            <Hits<GeoHit> hitComponent={(hit) =>
+              <StoreComponent
+                key={hit.hit.objectID}
+                store={hit.hit}
+                onClick={(_store) => setCurrentStore(_store)}
+                currentStore={currentStore}
+              />
+            } />
+          </div>
+
+          <div className="flex flex-col w-full">
+            <Map
+              currentStore={currentStore ? [currentStore._geoloc.lng, currentStore._geoloc.lat] : null}
+              onClickMarker={(storeCoordinate) => {}}
+            />
+          </div>
+        </div>
+      </InstantSearch>
     </div>
   );
 }
