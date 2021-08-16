ea4cd208c1714bbbf94ad1dcf366343920aa2807
Francis Rodrigues <francis.rod01@outlook.com>
2021-08-16 20:26:21 -0300

    feat: Implemented header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { ReactComponent as Logo } from '../../assets/logo.svg';
+
+const Header: React.FC<any> = ({ children }: { children?: JSX.Element[] | JSX.Element }) => {
+  return (
+    <header className={
+      'w-full bg-white px-5 py-3 shadow-lg flex items-center justify-between sm:flex-col md:flex-row'
+    }>
+      <Logo className={'w-auto h-16'} />
+      <div className="sm:w-full md:w-1/2">{children}</div>
+    </header>
+  );
+}
+
+export default Header;
